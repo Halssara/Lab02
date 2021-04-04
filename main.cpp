92e5077f0191687c4f999793c021b4f9cf9e5dea
Alice (IvanovOB) <zakaraka223@gmail.com>
2021-04-04 17:25:15 +0300

    code: add chastnoe

--- main.cpp
+++ main.cpp
@@ -9,5 +9,6 @@ int main()
     cin >> a >> b;
     cout << "sum = " << a + b << '\n'
     << "raznost` = " << a - b << '\n'
-    << "proizv = " << a * b << '\n';
+    << "proizv = " << a * b << '\n'
+    << "chastnoe = " << a / b << '\n';
 }
